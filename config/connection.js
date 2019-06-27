644eb27635ad5262c4694319e3bfbc126e8584ce
mtvalencia <mtv411@yahoo.com>
2019-06-26 20:56:40 -0600

    final

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "process.env.db_password",
+  password: "KuC00@$^",
   database: "burgers_db"
 });
 
